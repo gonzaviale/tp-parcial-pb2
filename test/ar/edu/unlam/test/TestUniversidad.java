63dde865b1d46b79282a85f0770f6aebf21c81f8
gonzaviale <gonzaloleonelviale@gmail.com>
2023-09-18 16:08:34 -0300

    Comienzo de proyecto

---
+++ test/ar/edu/unlam/test/TestUniversidad.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUniversidad {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
